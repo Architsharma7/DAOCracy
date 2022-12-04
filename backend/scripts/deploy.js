4c7cef0da2e9e243a9fb2f6f4383956bb16598f5
Saurabh Patil <55076843+Saurabhpatil-dev@users.noreply.github.com>
2022-12-04 06:45:25 +0530

    Verified Contract Address

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -7,6 +7,11 @@ async function main() {
   const verificationContract = await ethers.getContractFactory('Verification');
   const deployedVerificationContract = await verificationContract.deploy();
   await deployedVerificationContract.deployed();
+  console.log('Sleeping.....');
+  await sleep(50000);
+  await hre.run('verify:verify', {
+    address: deployedVerificationContract.address,
+  });
   console.log(
     `Verification contract deployed at ${deployedVerificationContract.address}`
   );
@@ -19,6 +24,12 @@ async function main() {
     deployedVerificationContract.address
   );
   await deployNFTMintingContract.deployed();
+  console.log('Sleeping.....');
+  await sleep(50000);
+  await hre.run('verify:verify', {
+    address: deployNFTMintingContract.address,
+    constructorArguments: [deployedVerificationContract.address],
+  });
   console.log(
     'NFTMinting contract is deployed at: ',
     deployNFTMintingContract.address
@@ -27,9 +38,16 @@ async function main() {
   /*
     Deployment of the Country DAO contract Address.
   */
+  const countryName = 'India';
   const countryDAOContract = await ethers.getContractFactory('MainDAO');
-  const deployCountryDAOContract = await countryDAOContract.deploy('INDIA');
+  const deployCountryDAOContract = await countryDAOContract.deploy(countryName);
   await deployCountryDAOContract.deployed();
+  console.log('Sleeping.....');
+  await sleep(50000);
+  await hre.run('verify:verify', {
+    address: deployCountryDAOContract.address,
+    constructorArguments: [countryName],
+  });
   console.log(
     'Country DAO contract is deployed at: ',
     deployCountryDAOContract.address
@@ -45,40 +63,24 @@ async function main() {
     deployNFTMintingContract.address
   );
   await deployUPStateDAOContract.deployed();
+  console.log('Sleeping.....');
+  await sleep(50000);
+  await hre.run('verify:verify', {
+    address: deployUPStateDAOContract.address,
+    constructorArguments: [
+      'Uttar Pradesh',
+      1,
+      deployNFTMintingContract.address,
+    ],
+  });
   console.log(
     'Uttar Pradesh State DAO contract is deployed at: ',
     deployUPStateDAOContract.address
   );
+}
 
-  /*
-    Deployment of the State DAO contract Address.
-  */
-  const kStateDAOContract = await ethers.getContractFactory('StateDAO');
-  const deployKStateDAOContract = await kStateDAOContract.deploy(
-    'Karnataka',
-    2,
-    deployNFTMintingContract.address
-  );
-  await deployKStateDAOContract.deployed();
-  console.log(
-    'Karnataka State DAO contract is deployed at: ',
-    deployKStateDAOContract.address
-  );
-
-  /*
-    Deployment of the State DAO contract Address.
-  */
-  const APStateDAOContract = await ethers.getContractFactory('StateDAO');
-  const deployAPStateDAOContract = await APStateDAOContract.deploy(
-    'Andhra Pradesh',
-    3,
-    deployNFTMintingContract.address
-  );
-  await deployAPStateDAOContract.deployed();
-  console.log(
-    'Andhra Pradesh State DAO contract is deployed at: ',
-    deployAPStateDAOContract.address
-  );
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 main()
@@ -87,3 +89,48 @@ main()
     console.error(error);
     process.exitCode = 1;
   });
+
+/*
+Sleeping.....
+Nothing to compile
+Successfully submitted source code for contract
+contracts/Verification.sol:Verification at 0x21D45f4C8102F9665B938Fafc471366E2F36dEE2
+for verification on the block explorer. Waiting for verification result...
+
+Successfully verified contract Verification on Etherscan.
+https://mumbai.polygonscan.com/address/0x21D45f4C8102F9665B938Fafc471366E2F36dEE2#code
+Verification contract deployed at 0x21D45f4C8102F9665B938Fafc471366E2F36dEE2
+Sleeping.....
+Compiled 15 Solidity files successfully
+Successfully submitted source code for contract
+contracts/NFTMinting.sol:NFTMinting at 0xBe89C0F04056F43b4397A4AC49149Df8A606F9f5
+for verification on the block explorer. Waiting for verification result...
+
+Successfully verified contract NFTMinting on Etherscan.
+https://mumbai.polygonscan.com/address/0xBe89C0F04056F43b4397A4AC49149Df8A606F9f5#code
+NFTMinting contract is deployed at:  0xBe89C0F04056F43b4397A4AC49149Df8A606F9f5
+Sleeping.....
+Nothing to compile
+Successfully submitted source code for contract
+contracts/MainDAO.sol:MainDAO at 0x0fe0FA841Fe426a7d2529b3dEB344fF8D1d41893
+for verification on the block explorer. Waiting for verification result...
+
+Successfully verified contract MainDAO on Etherscan.
+https://mumbai.polygonscan.com/address/0x0fe0FA841Fe426a7d2529b3dEB344fF8D1d41893#code
+Country DAO contract is deployed at:  0x0fe0FA841Fe426a7d2529b3dEB344fF8D1d41893
+Sleeping.....
+Nothing to compile
+Successfully submitted source code for contract
+contracts/StateDAO.sol:StateDAO at 0xFdcbbf41e9bc5043De844Be8Bbe7e723A787E534
+for verification on the block explorer. Waiting for verification result...
+
+Successfully verified contract StateDAO on Etherscan.
+https://mumbai.polygonscan.com/address/0xFdcbbf41e9bc5043De844Be8Bbe7e723A787E534#code
+Uttar Pradesh State DAO contract is deployed at:  0xFdcbbf41e9bc5043De844Be8Bbe7e723A787E534
+Sleeping.....
+Nothing to compile
+Successfully submitted source code for contract
+contracts/StateDAO.sol:StateDAO at 0xfCfa3Ae5AD5414cB8066C4dF50b9Be69645d9010
+for verification on the block explorer. Waiting for verification result...
+
+*/
